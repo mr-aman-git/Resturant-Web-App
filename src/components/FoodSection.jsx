31ba3a8dc26d6ccba75c7fd9a3fe1d8a5ede6a49
Aman Sharma <amanazr2003@gmail.com>
2025-01-25 19:22:02 +0530

    food section added

--- src/components/FoodSection.jsx
+++ src/components/FoodSection.jsx
@@ -4,7 +4,44 @@ import './FoodSection.css'
 const FoodSection = () => {
   return (
     <>
-    
+    <main>
+        <div className="food-section">
+            {/* FOOD BOX CONTAINER */}
+            <div className="food-box">
+                <img src="https://hinzcooking.com/wp-content/uploads/2023/03/karachi-biryani.jpg" alt="" />
+                <div className="food-content">
+                <h4>Biryani</h4>
+                <p>Biryani is a mixed rice dish popular in South Asia, made with rice, meat. </p>
+                <p><span>$80.00</span></p>
+                {/* <p><span></span></p> */}
+                </div>
+            </div>
+
+            <div className="food-box">
+                <img src="https://hinzcooking.com/wp-content/uploads/2023/03/karachi-biryani.jpg" alt="" />
+                <div className="food-content">
+                <h4>Biryani</h4>
+                <p>Biryani is a mixed rice dish popular in South Asia, made with rice, meat. </p>
+                <div className="price"><p>$80.00</p></div>
+                {/* <p><span></span></p> */}
+                </div>
+            </div>
+
+            <div className="food-box">
+                <img src="https://hinzcooking.com/wp-content/uploads/2023/03/karachi-biryani.jpg" alt="" />
+                <div className="food-content">
+                <h4>Biryani</h4>
+                <p>Biryani is a mixed rice dish popular in South Asia, made with rice, meat. </p>
+                <div className="price"><p>$80.00</p></div>
+                {/* <p><span></span></p> */}
+                </div>
+            </div>
+
+            {/* FOOD BOX CONTAINER */}
+
+        </div>
+
+    </main>
     </>
   )
 }
