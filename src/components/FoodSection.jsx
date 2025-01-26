2b595bd71d8aab22717d22f8ad10cce27e81f3af
Aman Sharma <amanazr2003@gmail.com>
2025-01-26 14:06:08 +0530

    some code removed

--- src/components/FoodSection.jsx
+++ src/components/FoodSection.jsx
@@ -19,24 +19,6 @@ const FoodSection = ({storeData}) => {
                 </div>
             </div>
 
-            <div className="food-box">
-                <img src="https://hinzcooking.com/wp-content/uploads/2023/03/karachi-biryani.jpg" alt="" />
-                <div className="food-content">
-                <h4>Biryani</h4>
-                <p>Biryani is a mixed rice dish popular in South Asia, made with rice, meat. </p>
-                <p><span>$80.00</span></p>
-                </div>
-            </div>
-
-            <div className="food-box">
-                <img src="https://hinzcooking.com/wp-content/uploads/2023/03/karachi-biryani.jpg" alt="" />
-                <div className="food-content">
-                <h4>Biryani</h4>
-                <p>Biryani is a mixed rice dish popular in South Asia, made with rice, meat. </p>
-                <p><span>$80.00</span></p>
-                </div>
-            </div>
-
             {/* FOOD BOX CONTAINER */}
 
         </div>
